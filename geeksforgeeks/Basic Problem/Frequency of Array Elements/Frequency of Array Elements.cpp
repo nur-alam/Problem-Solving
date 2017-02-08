4f8a3aea8a5206cc9302602e1d3309617d21d6da
nuralam <nuralam862@gmail.com>
2017-02-09 03:25:11 +0600

    solved frequency of array elements

---
+++ geeksforgeeks/Basic Problem/Frequency of Array Elements/Frequency of Array Elements.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n] = {0};
+        int k = n;
+        int p;
+
+        while(n--)
+        {
+            cin>>p;
+            arr[p-1]++;
+        }
+
+        for(int i=0; i<k; i++)
+        {
+            cout << arr[i] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+
+}
