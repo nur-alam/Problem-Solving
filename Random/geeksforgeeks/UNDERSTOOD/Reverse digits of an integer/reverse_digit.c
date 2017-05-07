6e3c173553bc6ce1f4c791dc86e392de1e07de76
nur-alam <nuralam862@gmail.com>
2017-05-07 22:27:39 +0600

    negative number handling

--- Random/geeksforgeeks/UNDERSTOOD/Reverse digits of an integer/reverse_digit.c
+++ Random/geeksforgeeks/UNDERSTOOD/Reverse digits of an integer/reverse_digit.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 //digit reversing function
 int reverseDigits(int num)
 {
+    //handling negative number
+   bool negativeFlag = false;
+   if(num<0)
+   {
+       negativeFlag = true;
+       num = -num;
+   }
    int rev_num = 0;
    while(num>0)
    {
        rev_num = rev_num*10 + num%10;
        num = num/10;
    }
-   return rev_num;
+   return negativeFlag ? -rev_num : rev_num;
 }
 
 int main(){
