a896bb083dae9e421fdb40c34ea0a52cbb5dd263
nur-alam <nuralam862@gmail.com>
2017-05-07 19:52:50 +0600

    reverse a number

---
+++ Random/geeksforgeeks/UNDERSTOOD/Reverse digits of an integer/reverse_digit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//digit reversing function
+int reverseDigits(int num)
+{
+   int rev_num = 0;
+   while(num>0)
+   {
+       rev_num = rev_num*10 + num%10;
+       num = num/10;
+   }
+   return rev_num;
+}
+
+int main(){
+    int num = 5896;
+    printf("%d",reverseDigits(num));
+    return 0;
+}
