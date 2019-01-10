aadd2bafb2e703d9275bff16dabd63804c96b75d
nur-alam <nuralam862@gmail.com>
2019-01-11 00:43:26 +0600

    codeforces two problems little modification

--- Codeforces/Anton and Letters/Anton and Letters.cpp
+++ Codeforces/Anton and Letters/Anton and Letters.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
     char ch = 'a'-'0';
 
--- Codeforces/Petya and Strings-112/Petya and Strings-112.c
+++ Codeforces/Petya and Strings-112/Petya and Strings-112.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 #include<string.h>
+
 int main()
 {
     char str_one[100];
     char str_two[100];
-    scanf("%s%s",str_one,str_two);//scanf faster than gets..if we need gets then use it otherwise should not use.
+
+    //scanf faster than gets..if we need gets then use it otherwise should not use.
+    scanf("%s%s",str_one,str_two);
+
     strlwr(str_one);
     strlwr(str_two);
+
     if(strcmp(str_one,str_two)==0)
         printf("0\n");
     else if(strcmp(str_one,str_two)==1)
         printf("1\n");
     else
         printf("-1\n");
+
     return 0;
 }
